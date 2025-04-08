0e9cdf2c2cf7a762ffc2a1593efea0660b827512
Asto Umbu <astoumbu001@gmail.com>
2025-04-08 15:43:27 +0800

    Website untuk melakukan sitsem jual beli

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("signup-link").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent default link behavior
+  window.location.href = "register.html"; // Redirect to the registration page
+});
